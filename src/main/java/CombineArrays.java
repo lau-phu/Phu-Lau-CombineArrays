10ff4a8c6100869b5b16a515cf3d3684e17c2837
lau-phu <plau123@ymail.com>
2023-03-02 20:38:53 +0000

    xxsacfsdxc

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,19 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        
+        int combined[] = new int[arr1.length+arr2.length];
+        int pos = 0;
+        for(int i = 0; i<arr1.length; i++){
+            combined[pos] = arr1[i];
+            pos++;
+        }
 
-        return null;
+        for(int i = 0; i<arr2.length; i++){
+            combined[pos] = arr2[i];
+            pos++;
+        }
+
+        return combined;
     }
 }
